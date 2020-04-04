0a0021790df1f804b35ded67944b6bedd014773c
svpino <svpino@svpino-macpro.local>
2020-04-04 19:11:42 -0400

    Object detection

--- Dockerfile
+++ Dockerfile
@@ -4,52 +4,48 @@ FROM tensorflow/tensorflow:${ARCHITECTURE}-py3
 RUN apt-get update && apt-get install -y --no-install-recommends \
         wget zip unzip git ca-certificates curl nginx
 
-# We need to install Protocol Buffers (Protobuf). Protobuf is Google's language and platform-neutral,  
-# extensible mechanism for serializing structured data. To make sure you are using the most updated code,
-# replace the linked release below with the latest version available on the Git repository.
-RUN curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.10.1/protoc-3.10.1-linux-x86_64.zip
-RUN unzip protoc-3.10.1-linux-x86_64.zip -d protoc3
+# We need to install Protocol Buffers (Protobuf). Protobuf is Google's language and 
+# platform-neutral, extensible mechanism for serializing structured data. To make sure you
+# are using the most updated code, replace the linked release below with the latest version
+# available on the Git repository.
+RUN curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip
+RUN unzip protoc-3.11.4-linux-x86_64.zip -d protoc3
 RUN mv protoc3/bin/* /usr/local/bin/
 RUN mv protoc3/include/* /usr/local/include/
 
-# Let's add the folder that we are going to be using to install all of our machine learning-related code
-# to the PATH. This is the folder used by SageMaker to find and run our code.
+# Let's add the folder that we are going to be using to install all of our machine 
+# learning-related code to the PATH. This is the folder used by SageMaker to find and run
+# our code.
 ENV PATH="/opt/ml/code:${PATH}"
 RUN mkdir -p /opt/ml/code
 WORKDIR /opt/ml/code
 
+COPY requirements.txt .
+
 RUN pip install --upgrade pip
 RUN pip install cython
-RUN pip install contextlib2
-RUN pip install pillow
-RUN pip install lxml
-RUN pip install matplotlib
-RUN pip install flask
-RUN pip install gevent
-RUN pip install gunicorn
-RUN pip install pycocotools
+RUN pip install -r requirements.txt
 
-# Let's now download Tensorflow from the official Git repository and install Tensorflow Slim from
-# its folder.
+# Let's now download Tensorflow from the official Git repository and install Tensorflow Slim.
 RUN git clone https://github.com/tensorflow/models/ tensorflow-models
 RUN pip install -e tensorflow-models/research/slim
 
-# We can now install the Object Detection API, also part of the Tensorflow repository. We are going to change
-# the working directory for a minute so we can do this easily.
+# We can now install the Object Detection API, also part of the Tensorflow repository. We are
+# going to change the working directory so we can do this easily.
 WORKDIR /opt/ml/code/tensorflow-models/research
 RUN protoc object_detection/protos/*.proto --python_out=.
 RUN python setup.py build
 RUN python setup.py install
 
-# If you are interested in using COCO evaluation metrics, you can tun the following commands to add the
-# necessary resources to your Tensorflow installation.
+# If you are interested in using COCO evaluation metrics, you can tun the following commands
+# to add the necessary resources to your Tensorflow installation.
 RUN git clone https://github.com/cocodataset/cocoapi.git
 WORKDIR /opt/ml/code/tensorflow-models/research/cocoapi/PythonAPI
 RUN make 
 RUN cp -r pycocotools /opt/ml/code/tensorflow-models/research/
 
-# Let's put the working directory back to where it needs to be, copy all of our code, and update the PYTHONPATH
-# to include the newly installed Tensorflow libraries.
+# Let's put the working directory back to where it needs to be, copy all of our code, and
+# update the PYTHONPATH to include the newly installed Tensorflow libraries.
 WORKDIR /opt/ml/code
 COPY /code /opt/ml/code
 ENV PYTHONPATH=${PYTHONPATH}:tensorflow-models/research:tensorflow-models/research/slim:tensorflow-models/research/object_detection
--- code/predictor.py
+++ code/predictor.py
@@ -1,87 +1,261 @@
 import os
-import logging
+import io
 import json
 import base64
+import hashlib
+import logging
+import logging.config
+import boto3
+import tempfile
+import numpy as np
 
-import flask
+from urllib.parse import urlparse
+from flask import Flask, request, Response
 
-from utils.tf_graph_util import TFGraph
+from PIL import Image
 
-prefix = '/opt/ml/'
-model_path = os.path.join(prefix, 'model')
-frozen_graph_path = os.path.join(model_path, 'graph/frozen_inference_graph.pb')
-label_path = os.path.join(model_path, 'graph/label_map.pbtxt')
-param_path = os.path.join(model_path, 'graph/hyperparameters.json')
+from model import Model
 
 
-# A singleton for holding the model. This simply loads the model and holds it.
-# It has a predict function that does a prediction based on the model and the input data.
+PREFIX_PATH = "/opt/ml/"
+CACHE_PATH = os.path.join(PREFIX_PATH, "cache")
+MODEL_PATH = os.path.join(PREFIX_PATH, "model")
+FROZEN_GRAPH_PATH = os.path.join(MODEL_PATH, "frozen_inference_graph.pb")
+LABEL_PATH = os.path.join(MODEL_PATH, "label_map.pbtxt")
+PARAM_PATH = os.path.join(MODEL_PATH, "hyperparameters.json")
 
 
-class ScoringService(object):
-    graph = None  # Where we keep the model when it's loaded
+class Configuration:
+    def __init__(self, data: dict):
+        self.file = data.get("file", None)
+        self.image = data.get("image", None)
+        self.stride = data.get("stride", 1)
+
+        self.cache = data.get("cache", False)
+        self.cache_id = data.get("cache_id", None)
+
+        self.aws_region = data.get("aws_region", None)
+        self.aws_access_key = data.get("aws_access_key", None)
+        self.aws_secret_access_key = data.get("aws_secret_access_key", None)
+
+
+class Cache:
+    def __init__(self, configuration: Configuration):
+        self.configuration = configuration
+        self.enabled = configuration.cache
+        self.path = CACHE_PATH
+
+    def get(self, key: str):
+        if self.enabled:
+            entry = os.path.join(self.path, f"{key}.npz")
+            if os.path.exists(entry):
+                data = np.load(entry, allow_pickle=True)
+                return data["data"]
+
+        return None
+
+    def put(self, key: str, value):
+        if self.enabled:
+            np.savez_compressed(os.path.join(self.path, f"{key}.npz"), data=value)
+
+
+class Processor:
+    model = None
     training_params = None
 
-    @classmethod
-    def get_training_params(cls):
-        if cls.training_params is None:
-            with open(param_path, 'r') as tc:
-                cls.training_params = json.load(tc)
+    @staticmethod
+    def factory(configuration: Configuration):
+        if configuration.image is not None or (
+            configuration.file is not None
+            and configuration.file[len(configuration.file) - 4 :] in (".jpg", ".png",)
+        ):
+            return ImageProcessor(configuration, Cache(configuration))
 
-        return cls.training_params
-
-    @classmethod
-    def get_graph(cls):
-        """Get the model object for this instance, loading it if it's not already loaded."""
-        if cls.graph is None:
-            cls.graph = TFGraph(label_path, frozen_graph_path)
-
-        return cls.graph
+        return None
 
     @classmethod
-    def predict(cls, image_data, image_size):
-        """For the input, do the predictions and return them."""
+    def get_training_params(self):
+        if self.training_params is None:
+            with open(PARAM_PATH, "r") as tc:
+                self.training_params = json.load(tc)
 
-        tf_graph = cls.get_graph()
-        inference_result = tf_graph.run_inference_for_single_image_from_bytes(image_data, image_size)
+        return self.training_params
 
-        return inference_result
+    @classmethod
+    def predict(self, image):
+        return self.get_model().inference(image)
+
+    @classmethod
+    def get_model(self):
+        if self.model is None:
+            self.model = Model(LABEL_PATH, FROZEN_GRAPH_PATH)
+
+        return self.model
+
+    def __init__(self, configuration: Configuration, cache: Cache):
+        self.configuration = configuration
+        self.cache = cache
+
+    def inference(self):
+        # This method should be implemented on the sub-classes.
+        pass
 
 
-# The flask app for serving predictions
-app = flask.Flask(__name__)
+class ImageProcessor(Processor):
+    def __init__(self, configuration: Configuration, cache: Cache):
+        super(ImageProcessor, self).__init__(configuration, cache)
+
+    def inference(self):
+        logging.info(f"Running inference on image...")
+        inference_cache_key = self.__get_key(self.__get_source(), "inference")
+
+        predictions = None
+
+        value = self.cache.get(inference_cache_key)
+        if value is not None:
+            predictions = value.tolist()
+            logging.debug(f"Inference from image found in cache")
+
+        if predictions is None:
+            logging.debug(f"Inference from image not found in cache")
+            image = self.__get_image()
+            inference = Processor.predict(image)
+
+            predictions = {"predictions": []}
+
+            for index, label in enumerate(inference["detection_classes"]):
+                box = inference["detection_boxes"][index].astype(float)
+                score = float(inference["detection_scores"][index])
+
+                ymin = box[0]
+                xmin = box[1]
+                ymax = box[2]
+                xmax = box[3]
+
+                prediction = [
+                    float(label - 1),
+                    score,
+                    xmin,
+                    ymin,
+                    xmax,
+                    ymax,
+                ]
+
+                predictions["predictions"].append(prediction)
+
+            self.cache.put(inference_cache_key, np.array(predictions))
+
+        return predictions
+
+    def __get_source(self):
+        if self.configuration.image is not None:
+            return base64.b64decode(self.configuration.image)
+
+        return self.configuration.file
+
+    def __get_image(self):
+        source_cache_key = self.__get_key(self.__get_source(), "source")
+
+        image = self.cache.get(source_cache_key)
+
+        if image is None:
+            logging.debug(f"Image not found in cache")
+
+            try:
+                if self.configuration.image is not None:
+                    image = self.__get_image_from_base64_string(self.__get_source())
+                else:
+                    fragments = urlparse(self.__get_source(), allow_fragments=False)
+                    if fragments.scheme == "s3":
+                        image = self.__get_image_from_s3(self.__get_source(), fragments)
+
+                self.cache.put(source_cache_key, image)
+            except Exception as e:
+                logging.error("There was an error handling image", e)
+        else:
+            logging.debug(f"Image found in cache")
+
+        return image
+
+    def __get_image_from_base64_string(self, source) -> np.array:
+        image = Image.open(io.BytesIO(source))
+        return self.__numpy(image)
+
+    def __get_image_from_s3(self, file, fragments) -> np.array:
+        logging.info(f"Downloading image from S3. Filename {file}...")
+
+        s3 = (
+            boto3.client(
+                "s3",
+                region_name=self.configuration.aws_region,
+                aws_access_key_id=self.configuration.aws_access_key,
+                aws_secret_access_key=self.configuration.aws_secret_access_key,
+            )
+            if self.configuration.aws_access_key
+            else boto3.client("s3")
+        )
+
+        tmp = tempfile.NamedTemporaryFile()
+
+        with open(tmp.name, "wb") as f:
+            s3.download_fileobj(fragments.netloc, fragments.path[1:], f)
+            return self.__numpy(Image.open(tmp.name))
+
+    def __get_key(self, file: str, suffix: str):
+        tmp = hashlib.md5(str(file).encode("utf8"))
+        tmp.update(str(suffix).encode("utf8"))
+        return tmp.hexdigest()
+
+    def __numpy(self, image):
+        (width, height) = image.size
+        return np.array(image.getdata()).reshape((height, width, 3)).astype(np.uint8)
 
 
-@app.route('/ping', methods=['GET'])
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+# gunicorn_error_logger = logging.getLogger('gunicorn.error')
+# app.logger.handlers.extend(gunicorn_error_logger.handlers)
+# app.logger.setLevel(logging.DEBUG)
+
+
+@app.route("/ping", methods=["GET"])
 def ping():
-    print('Ping received...')
+    """This endpoint determines whether the container is working and healthy.
+    We know everything is working appropriately if the model can be loaded
+    successfully.
+    """
+    logging.info("Ping received...")
 
-    """Determine if the container is working and healthy. In this sample container, we declare
-    it healthy if we can load the model successfully."""
-    health = ScoringService.get_graph() is not None  # You can insert a health check here
+    health = Processor.get_model() is not None
 
     status = 200 if health else 404
-    return flask.Response(response='\n', status=status, mimetype='application/json')
+    return Response(response="\n", status=status, mimetype="application/json")
 
 
-@app.route('/invocations', methods=['POST'])
-def invoke():
-    logging.info('Invoked with content_type {}'.format(flask.request.content_type))
+@app.route("/deleteme", methods=["POST"])
+def delete_me():
+    """
+    logging.info("Invoked with content_type {}".format(request.content_type))
 
-    if flask.request.content_type == 'application/json':
-        logging.info('Running inference on image...')
+    if request.content_type == "application/json":
+        logging.info("Running inference on image...")
 
-        body = flask.request.get_json()
-        image_data = base64.b64decode(body['image'])
+        body = request.get_json()
+        image_data = base64.b64decode(body["image"])
 
-        if 'threshold' in body:
-            threshold = body['threshold']
+        if "threshold" in body:
+            threshold = body["threshold"]
         else:
             threshold = 0.0
 
         training_params = ScoringService.get_training_params()
 
-        image_size = int(training_params['image_size']) if 'image_size' in training_params else 300
+        image_size = (
+            int(training_params["image_size"])
+            if "image_size" in training_params
+            else 300
+        )
 
         # run inference on image
         inference_result = ScoringService.predict(image_data, image_size)
@@ -89,9 +263,9 @@ def invoke():
         # convert inference result to json
         prediction_objects = []
         predictions = {}
-        detection_classes = inference_result['detection_classes']
-        detection_boxes = inference_result['detection_boxes']
-        detection_scores = inference_result['detection_scores']
+        detection_classes = inference_result["detection_classes"]
+        detection_boxes = inference_result["detection_boxes"]
+        detection_scores = inference_result["detection_scores"]
         for index, detection_class in enumerate(detection_classes):
             detection_box = detection_boxes[index].astype(float)
             detection_score = float(detection_scores[index])
@@ -103,13 +277,68 @@ def invoke():
             ymax = detection_box[2]
             xmax = detection_box[3]
 
-            prediction_object = [float(detection_class - 1), detection_score, xmin, ymin, xmax, ymax]
+            prediction_object = [
+                float(detection_class - 1),
+                detection_score,
+                xmin,
+                ymin,
+                xmax,
+                ymax,
+            ]
 
             prediction_objects.append(prediction_object)
 
-        predictions['prediction'] = prediction_objects
+        predictions["prediction"] = prediction_objects
 
-        return flask.Response(response=json.dumps(predictions), status=200, mimetype='application/json')
+        return Response(
+            response=json.dumps(predictions), status=200, mimetype="application/json"
+        )
+
+    return Response(
+        response='{"reason" : "Request is not application/x-image"}',
+        status=400,
+        mimetype="application/json",
+    )
+    """
+    pass
 
 
-    return flask.Response(response="{\"reason\" : \"Request is not application/x-image\"}", status=400, mimetype='application/json')
+@app.route("/invocations", methods=["POST"])
+def invoke():
+    """
+    TODO:
+
+    * Add support to provide http image
+    * Add support to provide local image (file instead of S3)
+    
+    * Add support to provide a threshold.
+
+    * Add support to provide a video: file | stride
+
+    * Implement gRPC interface
+
+    * What happens if there are no detections?
+    * Is the "return only these classes" working?
+    * What should we do with the hyperparameters.json?
+
+    * Add support to use different frozen models.
+
+
+    """
+
+    if request.content_type == "application/json":
+        configuration = Configuration(request.get_json())
+        processor = Processor.factory(configuration)
+        result = processor.inference()
+
+        logging.debug(f"Inference result {result}")
+
+        return Response(
+            response=json.dumps(result), status=200, mimetype="application/json",
+        )
+
+    return Response(
+        response='{"reason" : "Request is not application/x-image"}',
+        status=400,
+        mimetype="application/json",
+    )
